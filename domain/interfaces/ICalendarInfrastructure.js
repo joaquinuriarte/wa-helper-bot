7fa2c8a5682e53c3ac2147fe356d3f6fd615a5bb
Joaquin Uriarte <juriartebruno@gmail.com>
2025-05-21 15:30:11 -0700

    Type checking instead of explicit if/else

--- domain/interfaces/ICalendarInfrastructure.js
+++ domain/interfaces/ICalendarInfrastructure.js
@@ -5,18 +5,36 @@ const DomainEventResult = require('../models/DomainEventResult');
 const DomainEventUpdates = require('../models/DomainEventUpdates');
 
 class ICalendarInfrastructure {
+    /**
+     * Creates a new calendar event
+     * @param {DomainEvent} event - The event to create
+     */
     async createEvent(event) {
         throw new Error("Method 'createEvent()' must be implemented.");
     }
 
+    /**
+     * Fetches events based on query parameters
+     * @param {DomainEventQuery} query - The query parameters for fetching events
+     * @returns {Promise<DomainEventResult>} The query results
+     */
     async fetchEvents(query) {
         throw new Error("Method 'fetchEvents()' must be implemented.");
     }
 
+    /**
+     * Modifies an existing event
+     * @param {string} eventId - The ID of the event to modify
+     * @param {DomainEventUpdates} updates - The updates to apply to the event
+     */
     async modifyEvent(eventId, updates) {
         throw new Error("Method 'modifyEvent()' must be implemented.");
     }
 
+    /**
+     * Removes an event from the calendar
+     * @param {string} eventId - The ID of the event to remove
+     */
     async removeEvent(eventId) {
         throw new Error("Method 'removeEvent()' must be implemented.");
     }
--- domain/services/CalendarService.js
+++ domain/services/CalendarService.js
@@ -1,8 +1,4 @@
 // domain/services/CalendarService.js
-const DomainEvent = require('../models/DomainEvent');
-const DomainEventQuery = require('../models/DomainEventQuery');
-const DomainEventResult = require('../models/DomainEventResult');
-const DomainEventUpdates = require('../models/DomainEventUpdates');
 const ICalendarInfrastructure = require('../interfaces/ICalendarInfrastructure');
 
 class CalendarService {
@@ -17,17 +13,14 @@ class CalendarService {
     }
 
     async addEvent(event) {
-        // Ensure 'event' is of type DomainEvent if necessary, or trust the infrastructure layer to validate.
         return this._calendarInfrastructure.createEvent(event);
     }
 
     async getEvents(query) {
-        // Ensure 'query' is of type DomainEventQuery if necessary.
         return this._calendarInfrastructure.fetchEvents(query);
     }
 
     async updateEvent(eventId, updates) {
-        // Ensure 'updates' is of type DomainEventUpdates if necessary.
         return this._calendarInfrastructure.modifyEvent(eventId, updates);
     }
 
